f3b016b1f34db7d83e575bc25bcc185ee929e4e7
chenyq <1418806370@qq.com>
2019-02-15 15:47:03 +0800

    table

--- guns-admin/src/main/java/com/stylefeng/guns/modular/api/controller/ApiFaceController.java
+++ guns-admin/src/main/java/com/stylefeng/guns/modular/api/controller/ApiFaceController.java
@@ -2,31 +2,35 @@ package com.stylefeng.guns.modular.api.controller;
 
 import com.alibaba.fastjson.JSON;
 import com.baidu.aip.face.AipFace;
-import com.baomidou.mybatisplus.mapper.EntityWrapper;
 import com.stylefeng.guns.core.shiro.ShiroKit;
-import com.stylefeng.guns.core.util.DateUtil;
 import com.stylefeng.guns.modular.api.apiparam.ResponseData;
 import com.stylefeng.guns.modular.api.base.BaseController;
+import com.stylefeng.guns.modular.api.model.user.FaceInfoModel;
 import com.stylefeng.guns.modular.face.FaceMode;
 import com.stylefeng.guns.modular.face.FaceUser;
 import com.stylefeng.guns.modular.face.FaceUtil;
-import com.stylefeng.guns.modular.system.model.User;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Date;
 
 @RestController
 @RequestMapping("/api/apifacecontroller")
 public class ApiFaceController extends BaseController {
     private final Logger log = LoggerFactory.getLogger(ApiFaceController.class);
 
-
-    public ResponseData findImgBase64(String imgBase64) throws Exception {
-        ResponseData responseData = new ResponseData();
+    @RequestMapping(value = "/findImgBase64", method = RequestMethod.POST)
+    @ApiOperation("图片识别")
+    @ApiImplicitParams({
+            @ApiImplicitParam(required = true, name = "imgBase64", value = "识别图片base64字符串"),
+    })
+    public ResponseData<FaceInfoModel> findImgBase64(String imgBase64) throws Exception {
+        ResponseData<FaceInfoModel> responseData = new ResponseData();
         AipFace client = new AipFace(FaceUtil.APP_ID, FaceUtil.API_KEY, FaceUtil.SECRET_KEY);
         FaceUtil faceUtil = new FaceUtil();
         JSONObject user = faceUtil.findUser(client, imgBase64, ShiroKit.getUser().getDeptId() + "");
@@ -44,15 +48,24 @@ public class ApiFaceController extends BaseController {
             String deptId = faceUser.getGroup_id();
             String user_id = faceUser.getUser_id();
             Double score = faceUser.getScore();
-            if (score >= 60) {//图片相似率
+            if (score >= 80) {//图片相似率
                 //数据处理
                 //提示二次采集信息
-
+                String user_info = faceUser.getUser_info();
+                FaceInfoModel faceInfoModel = JSON.parseObject(user_info, FaceInfoModel.class);
+                faceInfoModel.setType(0);
+                faceInfoModel.setImgUrl("");
+                responseData.setDataCollection(faceInfoModel);
             }else {
                 //进行百度group注册
-                 user = faceUtil.userRegister(client,"",imgBase64,ShiroKit.getUser().getDeptId()+"","");
+//                 user = faceUtil.userRegister(client,"{name:\"张三\",idCard:\"15645456\",imgUrl:\"45314.jpg\"}",imgBase64,ShiroKit.getUser().getDeptId()+"","15645456");
                 //提示第一次采集
-
+                FaceInfoModel faceInfoModel=new FaceInfoModel();
+                faceInfoModel.setType(1);
+                faceInfoModel.setIdCard("");
+                faceInfoModel.setName("");
+                faceInfoModel.setImgUrl("");
+                responseData.setDataCollection(faceInfoModel);
             }
 
 //                    userAttendanceEntityWrapper.eq("")
@@ -60,6 +73,22 @@ public class ApiFaceController extends BaseController {
         } else {
             throw new Exception("暂无匹配数据");
         }
+        return responseData;
+    }
+
+    @RequestMapping(value = "/faceRegistered", method = RequestMethod.POST)
+    @ApiOperation("人脸注册")
+    @ApiImplicitParams({
+            @ApiImplicitParam(required = true, name = "imgBase64", value = "注册图片base64字符串"),
+            @ApiImplicitParam(required = true, name = "name", value = "姓名"),
+            @ApiImplicitParam(required = true, name = "idCard", value = "身份证"),
+    })
+    public ResponseData faceRegistered(String imgBase64,String name,String idCard){
+        ResponseData responseData=new ResponseData();
+        AipFace client = new AipFace(FaceUtil.APP_ID, FaceUtil.API_KEY, FaceUtil.SECRET_KEY);
+        FaceUtil faceUtil = new FaceUtil();
+        JSONObject jsonObject = faceUtil.userRegister(client, "{name:\""+name+"\",idCard:\""+idCard+"\",imgUrl:\"45314.jpg\" ,imgBase64:\""+imgBase64+"\"}", imgBase64, ShiroKit.getUser().getDeptId() + "", idCard);
+
         return responseData;
     }
 }
---
+++ guns-admin/src/main/java/com/stylefeng/guns/modular/api/model/user/FaceInfoModel.java
@@ -0,0 +1,59 @@
+package com.stylefeng.guns.modular.api.model.user;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel(value = "识别结果model")
+public class FaceInfoModel {
+    @ApiModelProperty("识别结果类型 0存在信息,1不存在信息")
+    private Integer type;
+    @ApiModelProperty("识别结果(身份证)")
+    private String idCard;
+    @ApiModelProperty("识别结果(姓名)")
+    private String name;
+    @ApiModelProperty("识别结果(头像地址)")
+    private String imgUrl;
+
+    @ApiModelProperty("识别结果(头像base64字符)")
+    private String imgBase64;
+
+    public String getImgBase64() {
+        return imgBase64;
+    }
+
+    public void setImgBase64(String imgBase64) {
+        this.imgBase64 = imgBase64;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
+    public String getIdCard() {
+        return idCard;
+    }
+
+    public void setIdCard(String idCard) {
+        this.idCard = idCard;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+}
